7769b1c31fa86fec09f36183208d04b21b5aa41c
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-06-23 17:03:23 +0530

    Conversion

---
+++ JavaScriptLerning/basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+
+let score = "false";
+// console.log(typeof (score));
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" --> 33
+//"33abd" --> not a number but type is number
+// true -> 1 false->0
+
+let isloggedIn = ""
+
+let bolleanISlog = Boolean(isloggedIn)
+console.log(bolleanISlog);
+
+// 1 --> true; 0 --> true
+// "" --> false
+// "kunal" --> true
+
+let sumNUmber = 33
+
+let s = String(sumNUmber)
+console.log(s);
+console.log(typeof s);
