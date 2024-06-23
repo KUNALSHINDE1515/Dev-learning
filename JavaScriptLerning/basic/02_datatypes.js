8de88252e30b8dd11edd8f143801572e8870fcad
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-06-23 16:48:28 +0530

    datatypes in js

---
+++ JavaScriptLerning/basic/02_datatypes.js
@@ -0,0 +1,30 @@
+"Use strict"; // treat all js codr as newer version 
+
+// alert("hello")  // we are using node js not browzer  
+
+console.log(5+9);   // code readability honi chahiye 
+
+let name = "kunal"
+let age = 18
+let isLoggegIn = true
+
+
+// number --> 2 to power of 53
+// bigint -->
+// String --> ""
+
+
+// boolran -->  True or False
+ 
+// null --> It is stand alone value.  -->repredented in
+// Undefined --> value not assign
+
+// symbol --> when you use react symbol is most of use // unique
+
+// object
+
+
+console.log(typeof undefined); // undefined hi ata hai
+console.log(typeof null);    //output --> object
+
+
