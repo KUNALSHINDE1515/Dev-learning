095f89fe982d19415077c59d5526afc321dd5141
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-03 16:00:03 +0530

    Object part- 2

---
+++ JavaScriptLerning/02_basic/04_object.js
@@ -0,0 +1,63 @@
+// This is non sigalton object
+
+const tinderUser = new Object()
+tinderUser.id = "123abs"
+tinderUser.name = "sammy"
+tinderUser.isloggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:'some@gmail.com',
+    fullname:{
+        userFullName:{
+            fName: "Kunal" ,
+            LName: "shinde"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userFullName.fName);
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3);
+
+
+// const obj3 = Object.assign({},obj1, obj2)
+
+const obj3  = {...obj1,...obj2}
+// console.log(obj3);
+
+
+const Users = [
+    {
+        id:1,
+        emai:"k@gmail.com"
+    },
+    {
+        id:1,
+        emai:"k@gmail.com"
+    },
+    {
+        id:1,
+        emai:"k@gmail.com"
+    },
+    {
+        id:1,
+        emai:"k@gmail.com"
+    },
+    {
+        id:1,
+        emai:"k@gmail.com"
+    }
+]
+
+Users[1].email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+console.log(tinderUser.hasOwnProperty.isloggedIn);
