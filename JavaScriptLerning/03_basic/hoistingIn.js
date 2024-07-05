9a7848f32cd29361a2aae5b2014adb7f0938f480
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-05 09:24:54 +0530

    hoisting in js

---
+++ JavaScriptLerning/03_basic/hoistingIn.js
@@ -0,0 +1,18 @@
+// debugger
+
+console.log(a);
+var a = 'kunal'
+
+hi()
+//function declaration
+function hi(){
+    console.log('heloo');
+}
+sayHi()
+// funcion expression
+var sayHi = function(){ // anyanmous function
+    console.log(`Hi, What are you doing`);
+}
+
+
+// IIFE
