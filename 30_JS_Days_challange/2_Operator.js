ff9892e721a18b0e367232823d16aacf4e3dbeca
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-15 09:40:10 +0530

    Day--> 2 OF 30 days

---
+++ 30_JS_Days_challange/2_Operator.js
@@ -0,0 +1,58 @@
+
+
+//  T -1 ---> Arithmetic Operation
+
+let num1 = 90
+let num2 = 85
+
+console.log(`Addition is : ${num1 + num2}`);   
+console.log(`Substract is : ${num1 - num2}`);   
+console.log(`Multiplication is : ${num1 * num2}`);   
+console.log(`divide is : ${num1 /num2}`);   
+console.log(`remider is : ${num1 % num2}`);   
+
+// T- 2  --->Assignment OPerator
+
+let num = 50
+num+= 50
+console.log(num);
+num-= 30
+console.log(num);
+
+
+// T3 -- Comparision OPerator
+
+let n1 = 50
+let n2 = 60
+
+console.log( n1 < n2);
+console.log( n1 > n2);
+console.log( n1 <= n2);
+console.log( n1 >= n2);
+console.log( n1 == n2);
+console.log( n1 === n2);
+
+
+
+//T-3 --> Logical OPerator
+
+let number = 30
+
+if (number % 2 === 0 && number>= 30) {
+    console.log("Condition match");
+}else{
+    console.log("Condition false");
+}
+
+if (number % 2 === 0 || number>= 28) {
+    console.log("Condition match");
+}else{
+    console.log("Condition false");
+}
+
+if (number != 28) {
+    console.log("not match");
+}
+
+
+
