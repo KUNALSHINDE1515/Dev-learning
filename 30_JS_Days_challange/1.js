86faaa92b478de1b03812447dffdd668571160b4
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-15 09:18:41 +0530

    Day--> 1 OF 30 days

---
+++ 30_JS_Days_challange/1.js
@@ -0,0 +1,38 @@
+
+// TAsk Activities
+
+// T --> 1
+
+var  UserNAme = "kunal"
+console.log(UserNAme);
+console.log( typeof UserNAme);
+let userAccount = 123456
+console.log(userAccount);
+console.log( typeof userAccount);
+
+
+
+//  T--> 2
+
+const isLoggedIN = false
+console.log(typeof isLoggedIN);
+
+let myArr = [1,2,3,4,"shuinde", "shubham", 23, 45]
+console.log(typeof myArr);
+
+let obj = {
+    name: "kunal",
+    age: 22
+}
+console.log(typeof obj);
+
+
+let stock = 222
+console.log(stock);
+stock = "deshmukh"
+console.log(stock);
+
+
+const name = "rohit"
+name = "shinde"
+// console.log(name);  // error --->   Assignment to constant variable.
