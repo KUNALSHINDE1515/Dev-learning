d0038f7d3ecdde1a64e63417dac00f7e1c081039
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-16 10:54:01 +0530

    guess the nub=mebr

---
+++ Basic_JS_Project/GuessNumber/script.js
@@ -0,0 +1,10 @@
+const InputNUmber = document.querySelector('.InputNUmber')
+
+const  guessNumber = Math.floor(Math.random() * 100);
+const h3 = document.querySelector('.result')
+
+const result = InputNUmber === guessNumber
+
+h3.innerTExt = result
+
+
