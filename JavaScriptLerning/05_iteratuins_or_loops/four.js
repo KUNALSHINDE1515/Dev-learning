402c100ab803f5ff895724a8c14c698ba37399fe
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-16 16:49:14 +0530

    higher order loops

---
+++ JavaScriptLerning/05_iteratuins_or_loops/four.js
@@ -0,0 +1,24 @@
+// const myOBJ = {
+//     JS: 'JAVASCRIPT',
+//     CPP: 'C++',
+//     PY:'PTYHON',
+//     RB:'RUBY'
+// }
+
+// for (const key in myOBJ) {
+//     console.log(`${key} shortcut is for ${myOBJ[key]}`);
+// }
+
+// const programing = ["js", "java", "pythons", "rb"]
+// for (const key in programing) {
+//     console.log(programing[key]);
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for (const key in map) {
+//   console.log(key);
+// }
