abd346ffa07042387aa7f316946754b2f1eb2af2
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-07-16 14:11:01 +0530

    changes' '

--- Basic_JS_Project/GuessNumber/script.js
+++
@@ -1,8 +0,0 @@
-const InputNUmber = document.querySelector('.InputNUmber')
-
-const  guessNumber = Math.floor(Math.random() * 100);
-const h3 = document.querySelector('.result')
-
-const result = InputNUmber === guessNumber
-
-
