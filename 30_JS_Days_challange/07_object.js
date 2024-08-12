a475953fae40610b0922cc9c706343a65bfc921e
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-08-12 14:59:22 +0530

    obect practice in js'

---
+++ 30_JS_Days_challange/07_object.js
@@ -0,0 +1,20 @@
+// Objects 
+// Activity --> 1 --> Object Creation and access
+
+// T->1
+
+const book = {
+    title: "Speak English Fast",
+    authoe:'Abdul salam chaus',
+    year: 2004
+}
+console.log(book);
+
+
+// T-->2 Author propeties access
+
+console.log(book.authoe);
+
+
+//Activity-->2 -->object methods
+
