8eb6eba82c8483e93b8c3332d5802ec0d83a4f03
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-08-18 14:41:48 +0530

    article preview

---
+++ FrontEndProject/article-preview-/script.js
@@ -0,0 +1,9 @@
+const share_Btn = document.getElementById('share-btn')
+console.log(share_Btn);
+
+share_Btn.addEventListener('click', () => {
+    share_Btn.setAttribute('class','share-btn-color')
+    
+})
+
+console.log("hiiiiiiiiiiiiiii");
