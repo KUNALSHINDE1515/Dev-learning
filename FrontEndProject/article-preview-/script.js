61a5ec8220c1aadd02b0eebb58834e81085c1605
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-08-21 12:23:38 +0530

    projecr practice

--- FrontEndProject/article-preview-/script.js
+++ FrontEndProject/article-preview-/script.js
@@ -1,9 +1,12 @@
 const share_Btn = document.getElementById('share-btn')
-console.log(share_Btn);
-
-share_Btn.addEventListener('click', () => {
-    share_Btn.setAttribute('class','share-btn-color')
-    
+share_Btn.addEventListener('click', (e) => {
+    share_Btn.setAttribute('class','share-btn-color');
+    togglediv();  
 })
+function togglediv(){   
+const div = document.getElementById('share-apps')
+    div.style.display = 'flex';
+    return div;
+}
 
-console.log("hiiiiiiiiiiiiiii");
+
