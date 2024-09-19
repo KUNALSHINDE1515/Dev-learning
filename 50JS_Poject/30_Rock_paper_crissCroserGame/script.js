d3b19df61ed324bf904bb66e5f241edced278385
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-09-19 06:46:35 +0530

    user said

--- 50JS_Poject/30_Rock_paper_crissCroserGame/script.js
+++ 50JS_Poject/30_Rock_paper_crissCroserGame/script.js
@@ -1,42 +1,78 @@
-const rock = document.getElementById('rock')
-const scissors = document.getElementById('scissors')
-const paper = document.getElementById('paper')
+const buttons = document.querySelectorAll("buttons")
 const result = document.getElementById('result')
 const YourScore = document.getElementById('YourScore')
 const comScore = document.getElementById('comScore')
 
-// console.log(rock,scissors,paper,result,YourScore,comScore);
-let score = 0
-let cscore = 0
-function check() {
-let myScore = Math.round(Math.random() * 3) + 1
-let compuScore = Math.round(Math.random() * 3) + 1
+let computerPlay ;
+let yourPlay;
 
-if (myScore === compuScore) {
-    result.innerText = "Its a tie"
-}else if (myScore > compuScore) {
-    result.innerText = "You win"
-    score = score + 1
-}else{
-    result.innerText ="Computer in win" 
-     cscore = cscore + 1 
-}
-}
-rock.addEventListener("click", () => {
-    check()
-    valueadd()
-})
-scissors.addEventListener("click", () => {
-    check()
-    valueadd()
-})
-paper.addEventListener("click", () => {
-    check()
-    valueadd()
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const result = PlaySection(button.id, computerPlay())
+        result.textContent = result
+    })
 })
 
-
-function valueadd() {
-    YourScore.innerHTML = `${score}`
-    comScore.innerHTML = `${cscore}`
+function ComputerPlay() {
+    const choise = ["rock", "paper" , "scrisscors"]
+    const result =   Math.floor(Math.random() * choise.length) 
+    return choise[result]
+    
 }
+
+function PlaySection(PlaySection , computerSection) {
+    
+    if (PlaySection === computerSection) {
+        return `Its atie !`
+    }else if(
+        (PlaySection === "rock" && computerSection ==="scrisscors")
+        (PlaySection === "paper" && computerSection ==="rock")
+        (PlaySection === "scrisscors" && computerSection ==="paper")
+    ){
+        yourPlay++
+        YourScore.textContent = yourPlay;
+        return `You win ! ${PlaySection} beats ${computerSection}`
+    }else{
+        computerPlay++
+        comScore.textContent = computerPlay
+        return ` You lose ! ${computerSection} beats ${PlaySection}`
+    }
+}
+
+
+
+// // console.log(rock,scissors,paper,result,YourScore,comScore);
+// let score = 0
+// let cscore = 0
+// function check() {
+// let myScore = Math.round(Math.random() * 3) + 1
+// let compuScore = Math.round(Math.random() * 3) + 1
+
+// if (myScore === compuScore) {
+//     result.innerText = "Its a tie"
+// }else if (myScore > compuScore) {
+//     result.innerText = "You win"
+//     score = score + 1
+// }else{
+//     result.innerText ="Computer in win" 
+//      cscore = cscore + 1 
+// }
+// }
+// rock.addEventListener("click", () => {
+//     check()
+//     valueadd()
+// })
+// scissors.addEventListener("click", () => {
+//     check()
+//     valueadd()
+// })
+// paper.addEventListener("click", () => {
+//     check()
+//     valueadd()
+// })
+
+
+// function valueadd() {
+//     YourScore.innerHTML = `${score}`
+//     comScore.innerHTML = `${cscore}`
+// }
