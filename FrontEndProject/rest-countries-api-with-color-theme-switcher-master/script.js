1f67a827fb8b6464f14d1bc4963bb33f922bb09a
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-10-01 15:49:06 +0530

    restFull-api-

--- FrontEndProject/rest-countries-api-with-color-theme-switcher-master/script.js
+++ FrontEndProject/rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,30 +1,60 @@
 const countriesContainer = document.querySelector('.conntries-container')
+const filterByResgion = document.querySelector('.filter')
+const searchInput = document.querySelector('.serach-container input')
+let allContriesData ;
 
 fetch('https://restcountries.com/v3.1/all').then( function (res) {
     return res.json()
 }).then((data) => {
-   data.map( function (country) {
-    // console.log(country);
-    
-    const countryCard = document.createElement('a')
-    countryCard.classList.add('country-card')
-    countryCard.href   = `./country.html?name=${country.name.common}`
-
-countryCard.innerHTML  = `
-    <img src="${country.flags.svg}" alt="flag">
-    <div class="card-text">
-        <h3 class="card-title">${country.name.common}</h3>
-        <p><b>Population : </b>${country.population.toLocaleString('en-IN')}</p>
-        <p><b>Region : </b>${country.region}</p>
-        <p><b>Capital : </b>${country.capital?.[0]}</p>
-    </div>
-`
+    rederCountries(data)
+    allContriesData = data
+})
 
 
-countriesContainer.append(countryCard)
-   })
-});
+filterByResgion.addEventListener('change', function(e){
+    // console.log(e.target.value);
+    fetch(`https://restcountries.com/v3.1/region/${filterByResgion.value}`).then( function (res) {
+        return res.json()
+    }).then((data) => {
+        rederCountries(data)
+    });
+})
 
 
+function rederCountries(data) {
+    countriesContainer.innerHTML = ''
+    data.map( function (country) {
+     // console.log(country);
+     
+     const countryCard = document.createElement('a')
+     countryCard.classList.add('country-card')
+     countryCard.href   = `./country.html?name=${country.name.common}`
+ 
+ countryCard.innerHTML  = `
+     <img src="${country.flags.svg}" alt="flag">
+     <div class="card-text">
+         <h3 class="card-title">${country.name.common}</h3>
+         <p><b>Population : </b>${country.population.toLocaleString('en-IN')}</p>
+         <p><b>Region : </b>${country.region}</p>
+         <p><b>Capital : </b>${country.capital?.[0]}</p>
+     </div>
+ `
+ 
+ 
+ countriesContainer.append(countryCard)
+    })
+}
+
+searchInput.addEventListener('input', (e) => {
+console.log(e.target.value);
+console.log(allContriesData);
+const saerchCountry = allContriesData.filter((country) =>  country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+console.log(saerchCountry);
+rederCountries(saerchCountry)
+})
 
 
+const themeChanger = document.querySelector('.theme-switcher')
+themeChanger.addEventListener("click", () => {
+    document.body.classList.toggle('dark')
+})
