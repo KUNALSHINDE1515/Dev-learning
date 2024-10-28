99cbcea54f0357998df503315df9ed4d3a27184b
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-10-28 17:19:56 +0530

    data json

--- FrontEndProject/product-list-with-cart-main/script.js
+++ FrontEndProject/product-list-with-cart-main/script.js
@@ -1,40 +1,58 @@
-const cartcontainer = document.querySelectorAll('.add-to-cart')
-cartcontainer.forEach(function(element) {
-    styledata(element)
+// const cartcontainer = document.querySelectorAll('.add-to-cart')
+// cartcontainer.forEach(function(element) {
+//     styledata(element)
+    
+// })
+
+// let data = 1
+
+
+// function styledata(element) {
+//     element.addEventListener("click", () => {
+//     element.style.backgroundColor ="orange";
+//     element.style.border ="orange";
+//     element.innerHTML =`
+//     <span id="decval">-</span>
+//     <span id="val">1</span>
+//     <span id="inCreval">+</span>
+//     `;
+    
+// const decVal = document.getElementById("decval")
+// const value = document.getElementById('val')
+// const inCreval = document.getElementById("inCreval")
+
+// decVal.addEventListener("click", () => {
+//     if (data === 0) {
+//         data = 0
+//     }else{
+//         data -= 1
+//     }
+// })
+// inCreval.addEventListener("click", () => {
+//     data += 1
+// })
+
+// value.innerText = data
+// })
+// }
+
+
+
+const heading = document.querySelectorAll('.text-con>h2')
+
+
+heading.forEach( (element , index) => {
+    console.log(element.innerText);
+    console.log(index);
+    
     
 })
 
-let data = 1
 
-
-function styledata(element) {
-    element.addEventListener("click", () => {
-    element.style.backgroundColor ="orange";
-    element.style.border ="orange";
-    element.innerHTML =`
-    <span id="decval">-</span>
-    <span id="val">1</span>
-    <span id="inCreval">+</span>
-    `;
+async function GetData() {
+    const data  = await fetch("./data.json")
+    const response = data.json()
+    console.log(response);
     
-const decVal = document.getElementById("decval")
-const value = document.getElementById('val')
-const inCreval = document.getElementById("inCreval")
-
-decVal.addEventListener("click", () => {
-    if (data === 0) {
-        data = 0
-    }else{
-        data -= 1
-    }
-})
-inCreval.addEventListener("click", () => {
-    data += 1
-})
-
-value.innerText = data
-})
 }
-
-
 
