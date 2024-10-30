430523142f1ed1e9bb5176e000a724cd59947e5d
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-10-30 17:05:08 +0530

    Json Get data

--- FrontEndProject/product-list-with-cart-main/script.js
+++ FrontEndProject/product-list-with-cart-main/script.js
@@ -1,58 +1,16 @@
-// const cartcontainer = document.querySelectorAll('.add-to-cart')
-// cartcontainer.forEach(function(element) {
-//     styledata(element)
-    
-// })
-
-// let data = 1
-
-
-// function styledata(element) {
-//     element.addEventListener("click", () => {
-//     element.style.backgroundColor ="orange";
-//     element.style.border ="orange";
-//     element.innerHTML =`
-//     <span id="decval">-</span>
-//     <span id="val">1</span>
-//     <span id="inCreval">+</span>
-//     `;
-    
-// const decVal = document.getElementById("decval")
-// const value = document.getElementById('val')
-// const inCreval = document.getElementById("inCreval")
-
-// decVal.addEventListener("click", () => {
-//     if (data === 0) {
-//         data = 0
-//     }else{
-//         data -= 1
-//     }
-// })
-// inCreval.addEventListener("click", () => {
-//     data += 1
-// })
-
-// value.innerText = data
-// })
-// }
-
-
-
-const heading = document.querySelectorAll('.text-con>h2')
-
-
-heading.forEach( (element , index) => {
-    console.log(element.innerText);
-    console.log(index);
-    
-    
-})
-
-
+const img = document.querySelector(".img>img")
+const headingofItem = document.querySelector(".text-con>h3")
+const Subheading = document.querySelector(".text-con>h2")
+const price = document.querySelector(".text-con>h1")
 async function GetData() {
     const data  = await fetch("./data.json")
-    const response = data.json()
-    console.log(response);
-    
+    const response = await data.json()
+    response.forEach((element ) => {
+      console.log(element);
+      img.src = element.image.desktop
+      headingofItem.innerText = element.category
+      Subheading.innerText = element.name
+      price.innerText = "$" + element.price
+})
 }
-
+GetData()
