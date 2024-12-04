32b54b9d202265de28476a99bf0b22a919cb7278
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-12-04 09:27:02 +0530

    Json data show and select each button

--- FrontEndProject/product-list-with-cart-main/script.js
+++ FrontEndProject/product-list-with-cart-main/script.js
@@ -1,16 +1,41 @@
-const img = document.querySelector(".img>img")
-const headingofItem = document.querySelector(".text-con>h3")
-const Subheading = document.querySelector(".text-con>h2")
-const price = document.querySelector(".text-con>h1")
+
+const cardcontainer = document.querySelector(".card-container")
 async function GetData() {
     const data  = await fetch("./data.json")
     const response = await data.json()
+    // console.log(response)
     response.forEach((element ) => {
-      console.log(element);
-      img.src = element.image.desktop
-      headingofItem.innerText = element.category
-      Subheading.innerText = element.name
-      price.innerText = "$" + element.price
+      const card = document.createElement("div")
+      card.innerHTML = `
+       <div class="cart">
+          <div class="img">
+            <img src="${element.image.desktop}" alt="img">
+          </div>
+          <div class="add-to-cart">
+            <img src="./assets/images/icon-add-to-cart.svg" alt="add-to=cart">
+            <h2>Add to cart</h2>
+          </div>
+          <div class="text-con">
+            <h3>${element.category}</h3>
+            <h2>${element.name}</h2>
+            <h1>$ ${element.price}</h1>
+          </div> 
+      `
+      cardcontainer.append(card)     
+
 })
 }
+
 GetData()
+
+const t = setTimeout(AddTocart , 1000)
+function AddTocart() {
+  const AddButton = document.querySelectorAll('.add-to-cart')
+  AddButton.forEach((element)  => {
+   element.addEventListener('click', () =>  console.log("Button Was Clicked"))
+  } )
+
+  
+}
+
+
