2084ae912ad4214d4cde6deb351004e24f1ddff2
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-12-19 11:12:03 +0530

    some practice on cart element

--- FrontEndProject/product-list-with-cart-main/script.js
+++ FrontEndProject/product-list-with-cart-main/script.js
@@ -3,22 +3,20 @@ const cardcontainer = document.querySelector(".card-container")
 async function GetData() {
     const data  = await fetch("./data.json")
     const response = await data.json()
-    // console.log(response)
-    response.forEach((element ) => {
+    response.forEach((data ) => {
       const card = document.createElement("div")
       card.innerHTML = `
        <div class="cart">
           <div class="img">
-            <img src="${element.image.desktop}" alt="img">
+            <img src="${data.image.desktop}" alt="img">
           </div>
           <div class="add-to-cart">
-            <img src="./assets/images/icon-add-to-cart.svg" alt="add-to=cart">
             <h2>Add to cart</h2>
           </div>
           <div class="text-con">
-            <h3>${element.category}</h3>
-            <h2>${element.name}</h2>
-            <h1>$ ${element.price}</h1>
+            <h3>${data.category}</h3>
+            <h2>${data.name}</h2>
+            <h1>$ ${data.price}</h1>
           </div> 
       `
       cardcontainer.append(card)     
@@ -28,14 +26,31 @@ async function GetData() {
 
 GetData()
 
+
 const t = setTimeout(AddTocart , 1000)
 function AddTocart() {
   const AddButton = document.querySelectorAll('.add-to-cart')
-  AddButton.forEach((element)  => {
-   element.addEventListener('click', () =>  console.log("Button Was Clicked"))
-  } )
-
-  
+  AddButton.forEach((button)  => {
+   button.addEventListener('click', () => {
+    let check = button.parentElement.lastElementChild
+    console.log(JSON.stringify(check));
+    
+   }) 
+  })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
